0efef70ba71a229425249b41d7f9f70533152894
MELYSSA SOUZA ARAUJO <126412768+Melyssa-Souza-Araujo@users.noreply.github.com>
2025-03-14 14:12:52 -0300

    Add files via upload

---
+++ exercicio_2.cs
@@ -0,0 +1,32 @@
+using System;
+using system.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercicio_1
+{
+    internal class Program 
+    {
+        static void Main(string[] args)
+        {
+            string nomeUsuario = " ";
+            char sexoUsuario = ' ';
+
+            Console.WriteLine("Insira seu nome: ");
+            nomeUsuario = Console.ReadLine();
+            Console.WriteLine("Insira seu sexo (F ou M): ");
+            sexoUsuario = Console.ReadLine();
+
+            if (sexoUsuario == 'F')
+            {
+                Console.WriteLine("Seu sexo é feminino");
+            
+            }
+            else
+            {
+                Console.WriteLine("Seu sexo é masculino");
+            }
+        }
+    }
+}
