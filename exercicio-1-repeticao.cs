30e8d652986a91f68a8f178b350c6aa786787985
MELYSSA SOUZA ARAUJO <126412768+Melyssa-Souza-Araujo@users.noreply.github.com>
2025-03-27 08:51:55 -0300

    Update exercicio-1-repeticao.cs

--- exercicio-1-repeticao.cs
+++ exercicio-1-repeticao.cs
@@ -11,36 +11,31 @@ namespace exercicio_1
         static void Main(string[] args)
         {
            const float PRECO_ALUNO = 0.30F;
-           const float PRECO_NAO_ALUNO = 0.50F;  
+           const float PRECO_NAO_ALUNO = 0.50F;
            float resultado = 0F;
-           char resposta = '';
-            
-            Console.WriteLine("Você é um aluno? Responda com sim ou não");
-            resposta = Convert.ToChar(Console.ReadLine());
-            
-            if (resposta == 'Sim')
-            {
-                for (int i = 1; i <= 100; i++)
-                {
-                    Console.WriteLine("Tabela de preço de Xerox para alunos");
-                    resultado = PRECO_ALUNO * i;
-                    Console.WriteLine(PRECO_ALUNO + " * " + i + " = " + resultado);
-                }
-            }
-            if (resposta == 'Nâo')
-            {
+           string resposta = "";
+
+           Console.WriteLine("Você é um aluno? Responda com sim ou não");
+           resposta = Console.ReadLine();
+
+           if (resposta == "sim")
+           {
                for (int i = 1; i <= 100; i++)
-                {
-                    Console.WriteLine("Tabela de preço de Xerox");
-                    resultado = PRECO_NAO_ALUNO * i;
-                    Console.WriteLine(PRECO_NAO_ALUNO + " * " + i + " = " + resultado);
-                } 
-            }
-            else
-            {
-                Console.WriteLine("Resposta inválida.");
-                Console.WriteLine("Saindo do programa...");
-            }
+               {
+                   Console.WriteLine("Tabela de preço de Xerox para alunos");
+                   resultado = PRECO_ALUNO * i;
+                   Console.WriteLine(PRECO_ALUNO + " * " + i + " = " + resultado);
+               }
+           }
+           if (resposta == "não")
+           {
+               for (int i = 1; i <= 100; i++)
+               {
+                   Console.WriteLine("Tabela de preço de Xerox");
+                   resultado = PRECO_NAO_ALUNO * i;
+                   Console.WriteLine(PRECO_NAO_ALUNO + " * " + i + " = " + resultado);
+               }
+           }
          }
     }
 }
