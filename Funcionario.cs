6230da5c691d86c815c1e6a2a9ab862554f71aa6
MELYSSA SOUZA ARAUJO <126412768+Melyssa-Souza-Araujo@users.noreply.github.com>
2025-05-08 09:32:31 -0300

    Add files via upload

---
+++ Funcionario.cs
@@ -0,0 +1,36 @@
+﻿using static System.Net.Mime.MediaTypeNames;
+using System;
+
+namespace aula_08_05
+{
+    internal class Funcionario
+    {
+        string nome;
+        string matricula;
+        static int contador;
+        public Funcionario(string nome, string matricula)
+                {
+                    Nome = nome;
+                    Matricula = matricula;
+                    AdicionarFuncionario();
+                }
+
+        public string Nome { get => nome; set => nome = value; }
+        public string Matricula { get => matricula; set => matricula = value; }
+        
+
+        public void Exibirdados()
+        { 
+            Console.WriteLine("Nome: " + Nome);
+            Console.WriteLine("Matricula: " + Matricula);
+            Console.WriteLine("Quantidade " + contador);
+        }
+        public void AdicionarFuncionario()
+        {
+            Console.WriteLine("Adicionando o funcionário " + ++contador);
+        }
+
+
+
+    }
+}
