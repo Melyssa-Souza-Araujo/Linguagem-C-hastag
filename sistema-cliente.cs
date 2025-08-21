35d8c8d769d122d16bb67a951b3b4f0433c9306e
MELYSSA SOUZA ARAUJO <126412768+Melyssa-Souza-Araujo@users.noreply.github.com>
2025-08-21 09:23:21 -0300

    Add files via upload

---
+++ sistema-cliente.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TEPOOManha
+{
+    internal class Program
+    {
+        static List<string> clientes = new List<string>();
+        static void Main(string[] args)
+        {
+            int opcao;
+            do
+            {
+                Console.WriteLine("=== SISTEMA DE CLIENTES (Monolítico) ===");
+                Console.WriteLine("1 - Cadastrar Cliente");
+                Console.WriteLine("2 - Listar Cliente");
+                Console.WriteLine("0 - Sair");
+
+                opcao = int.Parse(Console.ReadLine());
+
+                if (opcao == 1)
+                {
+                    Console.WriteLine("Digite o nome do cliente: ");
+                    string nome = Console.ReadLine();
+                    clientes.Add(nome);
+                    Console.WriteLine("Cliente cadastrado com sucesso!\n");
+                }
+                else if (opcao == 2){
+                    Console.WriteLine("\n--- Lista de Clientes ---");
+                    foreach (var c in clientes) 
+                    { 
+                        Console.WriteLine(c);
+                    }
+                    Console.WriteLine();
+                }
+            } while (opcao != 0);
+            Console.WriteLine("Encerrando o sistema...");
+        }
+    }
+}
